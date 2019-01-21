74436b7fbdb219e49494ebb5ca7034dd9811c5c8
Kumar Vavilala <s.kumar.vavilala@accenture.com>
2019-01-21 19:07:36 +0530

    Initial setup to add service and bind data to UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  items;
+  constructor(private dataService: DataService){
+    
+  }
+
+  ngOnInit(){
+    this.dataService.getPosts().subscribe(x => {this.items = x});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from  '@angular/common/http';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatCardModule } from  '@angular/material/card';
+import { MatButtonModule } from  '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
