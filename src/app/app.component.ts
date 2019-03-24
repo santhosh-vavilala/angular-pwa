2ec27413de64d52fedde68dc8ca5312ef9b67e69
Kumar Vavilala <s.kumar.vavilala@accenture.com>
2019-03-24 18:35:56 +0530

    Create subscription object and request for notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
+import { SwPush } from '@angular/service-worker';
+
+const VAPID_PUBLIC = 'BEOcNg4XUIDBokPhzy_5gqYHqdHcnnITtyZP8O4tULsFMTGaIjVenwRTbp97TsSrBh_IzOMOkrbxE-waSCFFFnk';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +12,18 @@ import { DataService } from './data.service';
 export class AppComponent {
   title = 'app';
   items;
-  constructor(private dataService: DataService){
-    
+  constructor(private dataService: DataService, private swPush: SwPush){
+    if (swPush.isEnabled) {
+      swPush
+        .requestSubscription({
+          serverPublicKey: VAPID_PUBLIC
+        })
+        .then(subscription => {
+          // send subscription to the server
+          console.log(subscription);
+        })
+        .catch(console.error);
+    }
   }
 
   ngOnInit(){
